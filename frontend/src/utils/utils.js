24a9733cb1f39b9c2517d6d85eed7f863ee660fa
Santosh Kumar Pradhan <147923912+santosh-kumar-pradhan@users.noreply.github.com>
2025-03-25 16:06:26 +0530

    Update base URL

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 export const axiosClient = axios.create({
-    baseURL:'http://localhost:1234/api/v1',
-    // baseURL:'https://open-job-app.onrender.com/api/v1' ,
+    // baseURL:'http://localhost:1234/api/v1',
+    baseURL:'https://open-job-app.onrender.com/api/v1'
 })
